1753eada5a8001d547ab4ac02177b44e2a3edc8b
tony555t <tonytullah764@gmail.com>
2023-02-14 00:34:31 +0300

    attribute

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
-## code your solution here. 
+# code your solution here. 
+require "pry"
+
+
+
+
+
+class Cat
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
+
+puss = Cat.new
+
+puss.name=("anisa")
+puss.meaw
+
+
+binding.pry
